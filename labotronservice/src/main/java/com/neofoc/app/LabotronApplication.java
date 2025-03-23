8fc4236e2f505864d7a59dd8efaa7cca4503fba4
Aantoine Samaha <ant1.samaha@gmail.com>
2025-03-23 18:16:29 +0200

    Getting started with a new application and version 2.0.0

---
+++ labotronservice/src/main/java/com/neofoc/app/LabotronApplication.java
@@ -0,0 +1,15 @@
+package com.neofoc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.neofoc.app", "com.neofoc.springboot"})
+public class LabotronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabotronApplication.class, args);
+	}
+
+}
