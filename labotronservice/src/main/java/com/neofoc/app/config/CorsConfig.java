d64444b92fe54881345d15f5b85b501fae92894d
Aantoine Samaha <ant1.samaha@gmail.com>
2025-03-30 09:55:15 +0300

    /foc/auth/login generates a token that is to be used in all /foc/** requests

--- labotronservice/src/main/java/com/neofoc/app/config/CorsConfig.java
+++ labotronservice/src/main/java/com/neofoc/app/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.neofoc.app.config;
 
+import com.neofoc.springboot.config.JwtAuthenticationFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -21,4 +23,19 @@ public class CorsConfig {
             }
         };
     }
+
+    @Bean
+    public FilterRegistrationBean<JwtAuthenticationFilter> jwtAuthenticationFilter() {
+        FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new JwtAuthenticationFilter());
+        registrationBean.addUrlPatterns("/foc/*");
+        registrationBean.setOrder(1);
+        registrationBean.setName("JwtAuthenticationFilter");
+
+        // Exclude the login endpoint
+        registrationBean.addInitParameter("excludeUrlPatterns", "/foc/auth/login");
+
+        return registrationBean;
+    }
+
 }
