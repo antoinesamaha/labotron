2e5a469ab7d774dd0421d3b1a7093d14036a9130
Aantoine Samaha <ant1.samaha@gmail.com>
2025-04-01 19:24:55 +0300

    Scripts to generate docker images

--- labotronservice/src/main/java/com/neofoc/app/LabotronApplication.java
+++ labotronservice/src/main/java/com/neofoc/app/LabotronApplication.java
@@ -2,11 +2,17 @@ package com.neofoc.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.neofoc.app", "com.neofoc.springboot"})
-public class LabotronApplication {
+public class LabotronApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LabotronApplication.class, args);
