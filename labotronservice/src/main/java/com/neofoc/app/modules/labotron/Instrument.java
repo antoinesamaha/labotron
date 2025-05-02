5b33154c43617526d0e9b2e8692210bb87937f0b
Aantoine Samaha <ant1.samaha@gmail.com>
2025-05-02 05:23:26 +0300

    Java changes

---
+++ labotronservice/src/main/java/com/neofoc/app/modules/labotron/Instrument.java
@@ -0,0 +1,37 @@
+package com.neofoc.app.modules.labotron;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+//@Cacheable
+@Table(name="instrument" )
+@Data
+public class Instrument implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected long       id ;
+	protected String     code ;
+	protected String     name ;
+	protected Boolean    connected ;
+	protected String     driverClassName ;
+	protected Boolean    onHold ;
+	protected String     comPort ;
+	protected Integer    comBaudRate ;
+	protected String     comParity ;
+	protected Integer    comDataBits ;
+	protected Integer    comStopBits ;
+	protected Boolean    isEmul ;
+
+	@OrderBy("lisTestLabel ASC")
+	@OneToMany(cascade=CascadeType.ALL, orphanRemoval=true, mappedBy = "instrument")
+//	@JoinColumn(name = "instrument_id")
+	//@NotFound(action = NotFoundAction.IGNORE)
+	protected Set<TestLabelMap> testLabelMaps = new HashSet<>();
+
+}
---
+++ labotronservice/src/main/java/com/neofoc/app/modules/labotron/TestLabelMap.java
@@ -0,0 +1,31 @@
+package com.neofoc.app.modules.labotron;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name="test_label_map" )
+@Data
+public class TestLabelMap implements Serializable {
+
+	//--- ENTITY PRIMARY KEY
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected long       id ;
+	protected String     lisTestLabel ;
+	protected String     instrumentTestCode ;
+	protected String     descrip ;
+	protected Integer    dayPriority ;
+	protected Integer    nightPriority ;
+	protected Integer    holidayPriority ;
+	protected Boolean    calculated ;
+	protected Boolean    onHold ;
+
+	@ManyToOne
+	protected TestGroup  testGroup ;
+
+	@ManyToOne
+	protected Instrument instrument ;
+}
