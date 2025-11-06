f57c99bf999d42fe2e835c9db255238855b7d47b
Aantoine Samaha <ant1.samaha@gmail.com>
2025-11-06 06:45:44 +0200

    security fix

--- labotron-service/src/main/java/com/neofoc/app/config/SecurityConfiguration.java
+++ labotron-service/src/main/java/com/neofoc/app/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,6 +13,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SecurityConfiguration {
