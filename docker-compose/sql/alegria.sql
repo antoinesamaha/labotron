5e105cbf6f83bdcf322ecba0039682730993b228
Aantoine Samaha <ant1.samaha@gmail.com>
2025-12-07 10:44:30 +0200

    Alegria tests sql

---
+++ docker-compose/sql/alegria.sql
@@ -0,0 +1,28 @@
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'ENAscreen', 1, 2, 'ENAscreen', '6594', 1, 0, NULL);
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'GBM', 1, 2, 'GBM', '6696', 1, 0, NULL);
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'ssDNA', 1, 2, 'ssDNA', '6586', 1, 0, NULL);
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'Gliadin A', 1, 2, 'Gliadin A', '6685', 1, 0, NULL);
+
+
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'MPO (pANCA)', 1, 2, 'MPO (pANCA)', '6743', 1, 0, NULL);
+
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'tTG A', 1, 2, 'tTG A', '6687', 1, 0, NULL);
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'tTG G', 1, 2, 'tTG G', '6686', 1, 0, NULL);
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'b-2-GPI G', 1, 2, 'b-2-GPI G', '6705', 1, 0, NULL);
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'b-2-GPI M', 1, 2, 'b-2-GPI M', '6706', 1, 0, NULL);
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'Cardiolip M', 1, 2, 'Cardiolip M', '6742', 1, 0, NULL);
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'Cardiolip G', 1, 2, 'Cardiolip G', '6581', 1, 0, NULL);
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'EBV(VCA) G', 1, 2, 'EBV(VCA) G', '6640', 1, 0, NULL);
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'EBV(VCA) M', 1, 2, 'EBV(VCA) M', '6639', 1, 0, NULL);
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'H pylori G', 1, 2, 'H pylori G', '6642', 1, 0, NULL);
+
+
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'Measles V G', 1, 2, 'Measles V G', '6707', 1, 0, NULL);
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'Measles V M', 1, 2, 'Measles V M', '6691', 1, 0, NULL);
+
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'Mumps V G', 1, 2, 'Mumps V G', '6649', 1, 0, NULL);
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'Mumps V M', 1, 2, 'Mumps V M', '6650', 1, 0, NULL);
+
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'Parvo G', 1, 2, 'Parvo G', '6652', 1, 0, NULL);
+INSERT INTO public.test_label_map (calculated, day_priority, descrip, holiday_priority, instrument_id, instrument_test_code, lis_test_label, night_priority, on_hold, test_group_id)VALUES(0, 1, 'Par B19 M abs', 1, 2, 'Par B19 M abs', '6653', 1, 0, NULL);
+
